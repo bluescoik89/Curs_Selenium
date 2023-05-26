d228919fa351a9b365a2e9e93f4b12b1a6798e53
bluescoik89 <stoicaionutcristian@gmail.com>
2023-05-26 16:51:36 +0300

    curs6 intermediar

---
+++ src/curs6/SwitchExample4.java
@@ -0,0 +1,43 @@
+package curs6;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class SwitchExample4 {
+    /*
+    Facem un program care verifica daca un student este eligibil de bursa
+    conditiile sunt:
+    sa fie anul 3
+    sa aiba un punctaj de 80 pentru bursa 1
+    sa aiba un punctaj de 50 pentru bursa 2
+     */
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Te rog introdu in ce an esti: ");
+        int an = scan.nextInt();
+        //System.out.println("Te rog introdu punctajul: ");
+        int punctaj; //= scan.nextInt();
+
+        switch (an) {
+            case 3:
+                System.out.println("Te rog introdu puctajul: ");
+                punctaj = scan.nextInt();
+                switch (punctaj) {
+                    case 80:
+                        System.out.println("Esti eligibil pentru bursa 1");
+                        break;
+                    case 50:
+                        System.out.println("Esti eligibil pentru bursa 2");
+                        break;
+                    default:
+                        System.out.println("Nu este eligibil pentru bursa");
+                }
+                break;
+            default:
+                System.out.println("Nu esti eligibil pentru bursa");
+
+        }
+
+    }
+}
