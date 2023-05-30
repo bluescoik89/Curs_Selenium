aae80ed6c9c63d7794b1fa49993af5853b136a40
bluescoik89 <stoicaionutcristian@gmail.com>
2023-05-30 16:37:06 +0300

    curs7 intermediar

---
+++ src/curs7/TestMostenire.java
@@ -0,0 +1,19 @@
+package curs7;
+
+public class TestMostenire {
+    public static void main(String[] args) {
+       // Tester obj = new Tester();
+       // obj.setEmail(null);
+
+        //Angajat -> Tester -> TesterAutomat
+
+        TesterAutomat obj = new TesterAutomat();
+        obj.setProgrammingLang("Java");  // clasa TesterAutomat
+        obj.setDepartment("QA");  // clasa Tester
+        obj.setSeniority("Junior"); //clasa Tester
+        obj.setEmail("Maria@maria.ro"); //clasa Angajat
+        obj.setNume("Maria"); //clasa Angajat
+
+
+    }
+}
